3cc7fb7a6f3412bf3edcb88b6de41a40f3e0bdcc
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-08-14 14:05:12 -0300

    feat: implementação inicial da view da pesquisa de pacotes

--- src/app/(public)/rastreador/tracker.view.tsx
+++ src/app/(public)/rastreador/tracker.view.tsx
@@ -10,11 +10,14 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
+import { Controller } from 'react-hook-form';
 
 import { LightTextField } from '@/app/(public)/rastreador/_components/light-text-field/LightTextField';
+import { useTrackerViewModel } from '@/app/(public)/rastreador/use-tracker.viewmodel';
 
 export function TrackerView() {
   const theme = useTheme();
+  const { navigateToPackageSearch, control, handleSubmit, errors } = useTrackerViewModel();
 
   return (
     <>
@@ -34,19 +37,34 @@ export function TrackerView() {
             <Grid item xs={12}>
               <Grid container>
                 <Grid xs={12} md={7} sx={{ margin: 'auto' }} item>
-                  <LightTextField
-                    size={'medium'}
-                    placeholder="Digite o código, por exemplo, ABC7869"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <IconButton edge="start" aria-label="Pesquisar pacote">
-                            <Search />
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
+                  <form onSubmit={handleSubmit(navigateToPackageSearch)}>
+                    <Controller
+                      control={control}
+                      name={'packageCode'}
+                      render={({ field }) => (
+                        <LightTextField
+                          {...field}
+                          error={!!errors?.packageCode?.message}
+                          helperText={errors?.packageCode?.message}
+                          size={'medium'}
+                          placeholder="Ex.: ABC7869"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <IconButton
+                                  edge="start"
+                                  aria-label="Pesquisar pacote"
+                                  onClick={handleSubmit(navigateToPackageSearch)}
+                                >
+                                  <Search />
+                                </IconButton>
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+                      )}
+                    />
+                  </form>
                 </Grid>
               </Grid>
             </Grid>
